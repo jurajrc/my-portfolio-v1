9c2ac6bdc90ce1e004711d43be5d258ecec8b6e8
jurajrc <81872592+jurajrc@users.noreply.github.com>
2021-11-26 17:53:45 +0100

    2. Create Nav.js

---
+++ src/components/Nav.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+//styled
+import styled from 'styled-components'
+// animation
+import { motion } from 'framer-motion'
+
+const Nav = () => {
+    return (
+        <Nav>
+            
+        </Nav>
+    )
+}
+
+const Nav = styled(motion.nav)`
+    
+`
+
+export default Nav
