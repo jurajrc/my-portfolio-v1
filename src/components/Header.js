f149029b934508c340a437d3907f52b09add2d97
jurajrc <81872592+jurajrc@users.noreply.github.com>
2021-11-26 21:28:26 +0100

    11. add img Heder.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,28 +4,56 @@ import { Link } from 'react-router-dom'
 import Nav from './Nav'
 // Style
 import styled from 'styled-components'
+import airplane from '../image/airplane1-s.png'
 // Animetion
+import { motion } from 'framer-motion'
 
 
 const Header = () => {
     return (
         <StyleHeader>
-            <div>
-                <div className="logo"></div>
-                <h2>Juraj Kubičár</h2>
-            </div>
+            <Flex >
+                <Link to="/" className="logo">
+                    <motion.img whileHover={{ rotate: -90 }} src={airplane} alt="lietdielko" />
+                </Link>
+                <h3>Juraj Kubičár</h3>
+            </Flex>
             <Nav />
         </StyleHeader>
     )
 }
 const StyleHeader = styled.header`
-    width: 100%;
+    max-width: 100%;
     height: 5em;
     background: #242124;
     display: flex;
     justify-content: space-between;
     align-items: center;
     position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    margin: 0 4%;
+    border-bottom: 1px solid #3e3e3e;
+
+    .logo {
+        width: 2.5em;
+        height: 2.5em;
+        background: #0080ff;
+        border-radius: 50%;
+        margin-right: 1em;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        img {
+            width: 80%;
+            height: 80%;
+        }
+    }
+`
+const Flex = styled(motion.div)`
+    display: flex;
+    align-items: center;
 `
 
 export default Header
