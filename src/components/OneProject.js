448c1316b852adb3889a00b755c9a283c71a8bae
jurajrc <81872592+jurajrc@users.noreply.github.com>
2021-11-30 17:53:49 +0100

    26. create OneProject.js

---
+++ src/components/OneProject.js
@@ -0,0 +1,15 @@
+import React from 'react'
+// Style
+import styled from 'styled-components'
+// Animation 
+import { motion } from 'framer-motion'
+
+const OneProject = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default OneProject
