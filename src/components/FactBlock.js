fd6f3b107ed4ec43439237672bd0bc1b28dbe9c0
jurajrc <81872592+jurajrc@users.noreply.github.com>
2021-12-01 20:10:23 +0100

    33. media FactBlock.js

--- src/components/FactBlock.js
+++ src/components/FactBlock.js
@@ -13,10 +13,21 @@ const StyleBlock = styled.div`
     display: flex;
     justify-content: space-between;
     border-bottom: 2px solid #3e3e3e;
+    padding-top: 7em;
+    @media (max-width: 1300px) {
+        padding-top: 6em;
+    }
+    @media (max-width: 900px) {
+        padding-top: 5em;
+    }
+    //padding-top: 2em;
     h2 {
         font-size: 2.187em;
         padding: 3% 0;
         color: #f8f8f8;
+        @media (max-width: 500px) {
+            font-size: 1.5em;
+        }
     }
     p {
         font-size: smaller;
