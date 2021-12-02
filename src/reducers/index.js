a03697f55d41bbede4ecab173c97e3aea0674985
jurajrc <81872592+jurajrc@users.noreply.github.com>
2021-12-02 13:57:04 +0100

    45. create showNav

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import openNavReducer from "./openNav";
+import showNavReducer from "./showNav";
 import { combineReducers } from "redux";
 
 // here add next State
 const allReducers = combineReducers({
     isOpenNav: openNavReducer,
+    isShowNav: showNavReducer,
 })
 
 export default allReducers;
---
+++ src/reducers/showNav.js
@@ -0,0 +1,10 @@
+const showNavReducer = (state = true, action) => {
+  switch (action.type) {
+        case 'toggleShowNav':
+            return !state
+        default:
+            return state
+  }
+}
+
+export default showNavReducer
