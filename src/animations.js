f9e78f5616f7115f36e7cb47c993615deddc77a3
jurajrc <81872592+jurajrc@users.noreply.github.com>
2021-12-03 22:59:39 +0100

    49. add animation tip2

--- src/animations.js
+++ src/animations.js
@@ -15,7 +15,8 @@ export const fadeIn = {
         opacity: 0,
         transition: {
             duration: 0.5,
-            ease: "easeOut"
+            ease: "easeOut",
+            then: "afterChildren",
         }
     }
 }
@@ -37,6 +38,54 @@ export const moveTextRight = {
     exit: {
         opacity: 0,
         y: "-50vw",
+        transition: {
+            duration: 1,
+        } 
+    }
+}
+
+export const fadeIn2 = {
+    hidden: {
+        opacity: 0,
+        transition: {
+            duration: 0.75,
+            ease: "easeOut"
+        }
+    },
+    show: {
+        opacity: 1,
+        then: "beforeChildren",
+        staggerChildren: 1,
+        duration: 2
+    },
+    exit: {
+        opacity: 0,
+        transition: {
+            duration: 0.5,
+            ease: "easeOut",
+            then: "afterChildren",
+        }
+    }
+}
+
+export const moveTextRight2 = {
+    hidden: {
+        opacity: 0,
+        x: "50vw",
+    },
+    show: {
+        opacity: 1,
+        x: "0%",
+        transition: {
+            duration: 1,
+            delay: 0.2,
+            type: 'spring',
+            stiffness: 200
+        }
+    },
+    exit: {
+        opacity: 0,
+        x: "-50vw",
         transition: {
             duration: 1,
         } 
