dbf378920c97a4fcd5cd3c8aeb7216a5e25650a7
jurajrc <81872592+jurajrc@users.noreply.github.com>
2021-12-05 20:58:12 +0100

    59. add popup  in animations

--- src/animations.js
+++ src/animations.js
@@ -35,6 +35,26 @@ export const titleAnim = {
 }
 
 
+export const popup = {
+    hidden: {
+        opacity: 0,
+        scale: 0.75,
+    },
+    show: {
+        opacity: 1,
+        scale: 1,
+        transition: {
+            duration: 0.7,
+        }
+    },
+    exit: {
+        opacity: 0,
+        scale: 0.75,
+        transition: {
+            duration: 0.7,
+        },
+    },
+}
 
 export const fade = {
     hidden: {
