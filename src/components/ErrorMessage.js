d2e4af2e23f4e4cbd01382d556ba5ebea01b9184
jurajrc <81872592+jurajrc@users.noreply.github.com>
2021-12-07 18:40:12 +0100

    63. Create error message

---
+++ src/components/ErrorMessage.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import fail from '../image/fail-200px.svg'
+// Style
+import { StyleSend, CenterSend } from '../styles'
+// Animation
+import { motion } from 'framer-motion'
+import { fadeIn, moveTextRight } from '../animations'
+
+const ErrorMessage = ({ toggleError }) => {
+    return (
+        <StyleSend variants={fadeIn} initial="hidden" animate="show" exit="exit" >
+            <CenterSend variants={moveTextRight} >
+                <img src={fail} alt="chack" />
+                <p>Nepodarilo sa Odoslať</p>
+                <motion.button whileHover={{
+                    textShadow: "0px 0px 4px #f8f8f8",
+                    boxShadow: "0px 0px 10px rgb(0, 125, 255)"
+                }} className="button" onClick={() => toggleError(false)} >Not OK</motion.button>
+            </CenterSend>
+        </StyleSend>
+    )
+}
+
+
+export default ErrorMessage
