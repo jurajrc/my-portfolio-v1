7449bf52b1bffbd70bf819f925f22c3aedd602ae
jurajrc <81872592+jurajrc@users.noreply.github.com>
2021-12-11 18:36:06 +0100

    90. import button scroll top

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Switch, Route, useLocation } from 'react-router-dom';
 import Header from './components/Header';
+import { useScrollPosition } from '@n8tb1t/use-scroll-position'
 // Components
 import AboutMy from './pages/AboutMy';
 import MyPortfolio from './pages/MyPortfolio';
 import Contact from './pages/Contact';
 import GlogalStyle from './components/GlobalStyle';
 import Footer from './components/Footer';
+import ScrollTopButton from './components/ScrollTopButton';
 // Animation
 import { AnimatePresence } from 'framer-motion'
 
 function App() {
+  const [showButtonTop, setShowButtonTop] = useState(false)
   const location = useLocation()
+
+  useScrollPosition(({ currPos }) => {
+    if(-currPos.y > 300) {
+      setShowButtonTop(true)
+    } else {
+      setShowButtonTop(false)
+    }
+  })
 
   return (
     <div className="App">
@@ -31,6 +42,11 @@ function App() {
         </Switch>
       </AnimatePresence>
 
+      <AnimatePresence>
+        { showButtonTop && (
+          <ScrollTopButton />
+        )}
+      </AnimatePresence>
       <Footer />
 
     </div>
