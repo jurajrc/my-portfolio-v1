51128f50d0245e4c6c58943b5398d00191d39a3d
jurajrc <81872592+jurajrc@users.noreply.github.com>
2021-12-14 23:23:21 +0100

    104. create DetailCertificate.js

---
+++ src/components/DetailCetrificate.js
@@ -0,0 +1,14 @@
+import React from 'react'
+// Style
+import styled from 'styled-components'
+
+
+const DetailCetrificate = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default DetailCetrificate
