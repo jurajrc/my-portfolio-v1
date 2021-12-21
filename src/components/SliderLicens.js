d2a8330de79365483a863f1409c1cc7029531555
jurajrc <81872592+jurajrc@users.noreply.github.com>
2021-12-21 10:13:17 +0100

    115. implement slider for Licences

--- src/components/SliderLicens.js
+++ src/components/SliderLicens.js
@@ -6,13 +6,17 @@ import Heading from './Heading'
 import 'react-alice-carousel/lib/alice-carousel.css'
 import styled from 'styled-components'
 // Animation
-import { motion } from 'framer-motion'
+import { motion, AnimatePresence } from 'framer-motion'
 // data
 import data from './dataCertificate'
+import DetailCetrificate from './DetailCetrificate'
+
 
 
 const SliderLicens = () => {
     const [allCertif] = useState(data)
+    const [oneCert, setOneCert] = useState(null)
+    const [openLightBox, setOpenLightBox] = useState(false)
 
     //console.table(allCertif);
     const handleDragStart = (e) => e.preventDefault();
@@ -23,13 +27,32 @@ const SliderLicens = () => {
         1028: { items: 3 }
     }
 
+    const showDetail = (e) => {
+      const handleId = e.target.id
+      const item = allCertif.find(one => handleId === one.id)
+      setOneCert(item)
+      setOpenLightBox(true)
+    }
+
     const itemss = allCertif.map(item => (
-        <img src={item.image} alt={item.alt} id={item.id} onDragStart={handleDragStart}/>
+        <img 
+            src={item.image} 
+            alt={item.alt} 
+            id={item.id} 
+            onDragStart={handleDragStart}
+            onClick={showDetail}
+        />
     ))
     
 
     return (
         <StyleCertificate>
+            <AnimatePresence>
+                {openLightBox && (
+                    <DetailCetrificate toggleShowDetail={setOpenLightBox} oneCert={oneCert}/>
+                )}
+            </AnimatePresence>
+
             <Heading heading="Cetifikaty a školenia" />
             <Slider>
                 <AliceCarousel 
@@ -41,13 +64,14 @@ const SliderLicens = () => {
                     infinite
                     controlsStrategy='alternate'
                     animationType='fadeout'
+                    disableButtonsControls
                 />
             </Slider>
         </StyleCertificate>
     )
 }
 const StyleCertificate = styled(motion.aside)`
-    padding-top: 2em;
+    padding: 2em 0 4em;
 `
 
 const Slider = styled.div`
@@ -57,10 +81,14 @@ const Slider = styled.div`
     justify-content: center;
     margin: 0 auto;
     margin-top: 4em;
+    @media (max-width: 600px) {
+        margin-top: 2em;
+    }
 
     img {
         width: 60%;
         height: auto;
+        cursor: pointer;
     }
 `
 
