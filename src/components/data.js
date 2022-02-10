18bb3bae8ade51d15d4013b06c064b1a204f9c36
jurajrc <81872592+jurajrc@users.noreply.github.com>
2022-02-10 21:57:42 +0100

    126. add content in data

--- src/components/data.js
+++ src/components/data.js
@@ -1,6 +1,6 @@
 import { v4 as uuidv4 } from 'uuid'
 
-//images
+//image
 import stolar2 from '../image/small/img-stolarstvo-v2-s.png'
 import ignite from '../image/small/img-ignite-s.png'
 import weather from '../image/small/img-weacher-s.png'
@@ -10,6 +10,9 @@ import space_web from '../image/small/img-space-s.png'
 import jurajrc from '../image/small/img-jurajrc-s.png'
 import stolar1 from '../image/small/img-stolarstvo-v1-s.png'
 
+// images
+
+
 function allgallery () {
     return [
         {
@@ -18,61 +21,120 @@ function allgallery () {
             url: "/portfolio/web6",
             http: "http://stolarstvojos.6f.sk/#/",
             image: stolar2,
-            github: "https://github.com/jurajrc/stolarstvoJOS.git"
+            images: [],
+            github: "https://github.com/jurajrc/stolarstvoJOS.git",
+            description: "Prvá verzia tejto stránky bola robená v PHP, po naučení Reaktu som mal chuľ ju hneď prekodiť, malá zmena dizajnu a na oživenie pridal animácie cez Framer Motion",
+            package: ["React", "Redux", "Styled component", "Framer motion"],
+            technologies: [],
+            use_tech: 'React'
         },
         {
             name: "Ignite",
             id: uuidv4(),
-            url: "gallery/web5",
+            url: "/portfolio/web5",
             http: "http://juraj2021.6f.sk/",
             image: ignite,
-            github: "https://github.com/jurajrc/ignite-react-redux.git"
+            images: [],
+            github: "https://github.com/jurajrc/ignite-react-redux.git",
+            description: "Aplikácia na vyhľadávanie detailov hry, kde môžu používatelia zobraziť obľúbené, nové a pripravované hry a vyhľadať hru. Vyvinuté na zvýšenie mojich skúseností s ReactJS, Redux, Framer Motion a zhromažďovaním a manipuláciou s údajmi API pomocou Axios.",
+            package: ["React", "Styled component", "Framer motion", "Redux", "Axios"],
+            technologies: [],
+            use_tech: 'React'
         },
         {
             name: "Weather",
             id: uuidv4(),
-            url: "gallery/web4",
+            url: "/portfolio/web4",
             http: "https://weather.6f.sk/",
-            image: weather
+            image: weather,
+            images: [],
+            github: "https://github.com/jurajrc/weather-app",
+            description: "Je to vlastná aplikácia kde si uživateľ môže pozrieť aktuálne počasie v mestách celého sveta. Vyvinuté na zvíšenie mojich skúseností v ReactJS, Framer Motion a zhromaždovanim a manipiláciou s údajmi API pomocou Axios",
+            package: [],
+            technologies: [],
+            use_tech: 'React'
         },
         {
             name: "Tunes",
             id: uuidv4(),
-            url: "gallery/web3",
+            url: "/portfolio/web3",
             http: "http://letlomnica.6f.sk/",
             image: tunes,
-            github: "https://github.com/jurajrc/Tunes-TypeScript.git"
+            images: [],
+            github: "https://github.com/jurajrc/Tunes-TypeScript.git",
+            description: "Aplikacia robaná v kurze v Lern2Code kde prví krát stretol s TypeSriptom a ťahať dáta cez API volanie ",
+            package: ["React", "Typescript", "Styled component"],
+            technologies: [],
+            use_tech: 'React'
         },
         {
             name: "Player",
             id: uuidv4(),
-            url: "gallery/web2",
+            url: "/portfolio/web2",
             http: "http://daniellaart.6f.sk/",
             image: player,
-            github: "https://github.com/jurajrc/Player-react.git"
+            images: [],
+            github: "https://github.com/jurajrc/Player-react.git",
+            description: "V aplikácii hudobného prehrávača môžu používatelia prehrávať skladbu z knižnice a povoliť tmavý režim. Vyvinuté na zvýšenie mojich skúseností s ReactJS a modernými háčikmi.",
+            package: [],
+            technologies: [],
+            use_tech: 'React'
         },
         {
-            name: "frontend",
+            name: "Space Web",
             id: uuidv4(),
-            url: "gallery/web1.1",
+            url: "/portfolio/web1.1",
             http: "https://juraj-web2.6f.sk/",
-            image: space_web
-        },
-        {
-            name: "JurejRC v1",
-            id: uuidv4(),
-            url: "gallery/web1",
-            http: "http://www.jurajrc.sk/",
-            image: jurajrc
+            image: space_web,
+            images: [],
+            github: "https://github.com/jurajrc/exercise7-app-typescript",
+            description: "Cvičenie zo stránky frontend-mentor, nakódiť web podla dizajnu robený vo Figme, tu som si precvičil React Typescript a Redux",
+            package: ["React", "Styled component", "Typescript", "Framer motion", "Redux"],
+            technologies: [],
+            use_tech: 'React'
         },
         {
             name: "Stolarstvo JOS v1",
             id: uuidv4(),
-            url: "gallery/web0",
+            url: "/portfolio/web0",
             http: "http://matejo.6f.sk/",
-            image: stolar1
+            image: stolar1,
+            images: [],
+            github: "",
+            description: "",
+            package: [],
+            technologies: [
+                "https://s2.svgbox.net/files.svg?ic=vscode&color=474724",
+                "https://s2.svgbox.net/files.svg?ic=html&color=000",
+                "https://s2.svgbox.net/files.svg?ic=scss&color=000",
+                "https://s2.svgbox.net/files.svg?ic=js",
+                "https://s2.svgbox.net/files.svg?ic=php3"
+            ],
+            use_tech: 'php'
         },
+        {
+            name: "JurajRC php",
+            id: uuidv4(),
+            url: "/portfolio/web1",
+            http: "http://www.jurajrc.sk/",
+            image: jurajrc,
+            images: [],
+            github: "",
+            description: "Môj osobný web spojený s koníčkom, tu som si odskúšal svoje prvé skúsenosti fromtend vývojom a rozkuskovať kód vďaka PHP.",
+            package: [],
+            technologies: [
+                "https://s2.svgbox.net/files.svg?ic=vscode&color=474724",
+                "https://s2.svgbox.net/files.svg?ic=html&color=000",
+                "https://s2.svgbox.net/files.svg?ic=scss&color=000",
+                "https://s2.svgbox.net/files.svg?ic=js",
+                "https://s2.svgbox.net/files.svg?ic=php3"
+            ],
+            use_tech: 'php'
+        },
+        
     ]
 }
+
+
 
 export default allgallery
